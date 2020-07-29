a0f390eebada95a9a501ae05ba725eaae3370e1a
SB-Jr <shrijitbasak@gmail.com>
2020-07-29 17:54:54 +0530

    Basic project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementing models to solve the mnist dataset with various parameters. Comparing the result of the models under various conditions. A test playground to test new models before implementing it on actual large datasets.',
+    author='Shrijit Basak (SB-Jr, shrijitbasak@gmail.com) ',
+    license='GPLv3',
+)
